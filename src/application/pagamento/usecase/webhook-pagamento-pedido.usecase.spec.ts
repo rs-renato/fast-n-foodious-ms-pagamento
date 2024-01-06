5903dd38ef235b3297cc02d6aa9549ad7cd7a369
Renato Rodrigues <spamcares-github@yahoo.com>
2024-01-06 14:06:29 -0300

    - removendo block de testes para finalização de IaC - Implementação de testes que haviam sido comentados/nao implementados.

--- src/application/pagamento/usecase/webhook-pagamento-pedido.usecase.spec.ts
+++ src/application/pagamento/usecase/webhook-pagamento-pedido.usecase.spec.ts
@@ -1,108 +1,112 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { PagamentoProviders } from 'src/application/pagamento/providers/pagamento.providers';
-// import { WebhookPagamentoValidator } from 'src/application/pagamento/validation/webhook-pagamento.validator';
-// import { PedidoProviders } from 'src/application/pedido/providers/pedido.providers';
-// import { BuscarPedidoPorIdUseCase, EditarPedidoUseCase } from 'src/application/pedido/usecase';
-// import { ServiceException } from 'src/enterprise/exception/service.exception';
-// import { ValidationException } from 'src/enterprise/exception/validation.exception';
-// import { EstadoPagamento } from 'src/enterprise/pagamento/enums/pagamento.enums';
-// import { Pagamento } from 'src/enterprise/pagamento/model/pagamento.model';
-// import { EstadoPedido } from 'src/enterprise/pedido/enums/pedido';
-// import { Pedido } from 'src/enterprise/pedido/model/pedido.model';
-// import { IRepository } from 'src/enterprise/repository/repository';
-// import { PersistenceInMemoryProviders } from 'src/infrastructure/persistence/providers/persistence-in-memory.providers';
-// import { PagamentoConstants, PedidoConstants } from 'src/shared/constants';
-// import { WebhookPagamentoPedidoUseCase } from './webhook-pagamento-pedido.usecase';
-//
-// describe('WebhookPagamentoPedidoUseCase', () => {
-//    let useCase: WebhookPagamentoPedidoUseCase;
-//    let editarPedidoUseCase: EditarPedidoUseCase;
-//    let buscarPedidoPorIdUseCase: BuscarPedidoPorIdUseCase;
-//    let pagamentoRepository: IRepository<Pagamento>;
-//    let validators: WebhookPagamentoValidator[];
-//
-//    const transacaoId = 'testTransacaoId';
-//    const estadoPagamento = EstadoPagamento.CONFIRMADO;
-//    const pagamento: Pagamento = {
-//       pedidoId: 1,
-//       transacaoId: '123-abc',
-//       estadoPagamento: EstadoPagamento.PENDENTE,
-//       total: 100,
-//       dataHoraPagamento: new Date(),
-//       id: 1,
-//    };
-//    const pedido: Pedido = {
-//       clienteId: 1,
-//       dataInicio: '2023-08-26',
-//       estadoPedido: EstadoPedido.PAGAMENTO_PENDENTE,
-//       ativo: true,
-//       id: 1,
-//       total: 100,
-//    };
-//
-//    beforeEach(async () => {
-//       const module: TestingModule = await Test.createTestingModule({
-//          providers:
-//       }).compile();
-//
-//       // Desabilita a saída de log
-//       module.useLogger(false);
-//
-//       useCase = module.get<WebhookPagamentoPedidoUseCase>(PagamentoConstants.WEBHOOK_PAGAMENTO_PEDIDO_USECASE);
-//       editarPedidoUseCase = module.get<EditarPedidoUseCase>(PedidoConstants.EDITAR_PEDIDO_USECASE);
-//       buscarPedidoPorIdUseCase = module.get<BuscarPedidoPorIdUseCase>(PedidoConstants.BUSCAR_PEDIDO_POR_ID_USECASE);
-//       pagamentoRepository = module.get<IRepository<Pagamento>>(PagamentoConstants.IREPOSITORY);
-//       validators = module.get<WebhookPagamentoValidator[]>(PagamentoConstants.WEBHOOK_PAGAMENTO_VALIDATOR);
-//    });
-//
-//    describe('webhook', () => {
-//       it('deve processar com sucesso um pagamento válido', async () => {
-//          const result = await useCase.webhook(transacaoId, estadoPagamento);
-//          expect(result).toBe(true);
-//       });
-//
-//       it('should handle an invalid payment state', async () => {
-//          // Mock the payment validators
-//          validators = []; // Add any necessary validators for invalid payments
-//
-//          await expect(useCase.webhook(transacaoId, 999)).rejects.toThrowError(ValidationException);
-//       });
-//
-//       it('should handle a missing payment', async () => {
-//          // Mock the payment repository to return no payment
-//          (pagamentoRepository.findBy as jest.Mock).mockResolvedValue([]);
-//
-//          await expect(useCase.webhook(transacaoId, estadoPagamento)).rejects.toThrowError(ServiceException);
-//       });
-//
-//       it('should handle a missing associated order', async () => {
-//          // Mock the order repository to return no order
-//          (buscarPedidoPorIdUseCase.buscarPedidoPorId as jest.Mock).mockResolvedValue(undefined);
-//
-//          await expect(useCase.webhook(transacaoId, estadoPagamento)).rejects.toThrowError(ServiceException);
-//       });
-//
-//       it('should handle a successful payment with CONFIRMADO state', async () => {
-//          // Mock the payment validators
-//          validators = []; // Add any necessary validators for valid payments
-//
-//          // Mock the payment state as CONFIRMADO
-//          const confirmedEstadoPagamento = EstadoPagamento.CONFIRMADO;
-//          const confirmedPedido: Pedido = {
-//             ...pedido,
-//             estadoPedido: EstadoPedido.PAGAMENTO_PENDENTE, // Assuming initial state
-//          };
-//
-//          // Mock the order repository to return the order
-//          (buscarPedidoPorIdUseCase.buscarPedidoPorId as jest.Mock).mockResolvedValue(confirmedPedido);
-//
-//          const result = await useCase.webhook(transacaoId, confirmedEstadoPagamento);
-//
-//          expect(result).toBe(true);
-//          expect(confirmedPedido.estadoPedido).toBe(EstadoPedido.RECEBIDO);
-//       });
-//    });
-// });
+import { Test, TestingModule } from '@nestjs/testing';
+import { WebhookPagamentoPedidoUseCase } from './webhook-pagamento-pedido.usecase';
+import { EstadoPagamento } from 'src/enterprise/pagamento/enum/estado-pagamento.enum';
+import { Pagamento } from 'src/enterprise/pagamento/model/pagamento.model';
+import { IRepository } from 'src/enterprise/repository/repository';
+import { ServiceException } from 'src/enterprise/exception/service.exception';
+import { ValidationException } from 'src/enterprise/exception/validation.exception';
+import { BuscaPedidoIdUseCase } from './busca-pedido-id.usecase';
+import { AtualizaPedidoComoRecebidoUseCase } from 'src/application/pagamento/usecase/atualiza-pedido-como-recebido.usecase';
+import { PagamentoConstants } from 'src/shared/constants';
+import { WebhookPagamentoValidator } from 'src/application/pagamento/validation/webhook-pagamento.validator';
+
+describe('WebhookPagamentoPedidoUseCase', () => {
+   let useCase: WebhookPagamentoPedidoUseCase;
+   let repository: IRepository<Pagamento>;
+   let buscaPedidoIdUseCase: BuscaPedidoIdUseCase;
+   let atualizaPedidoComoRecebidoUseCase: AtualizaPedidoComoRecebidoUseCase;
+
+   const mockedValidators: WebhookPagamentoValidator[] = []; // Adicione os validadores conforme necessário
+
+   const mockedPagamento: Pagamento = {
+      pedidoId: 1,
+      transacaoId: '123',
+      estadoPagamento: EstadoPagamento.CONFIRMADO,
+      total: 100,
+      dataHoraPagamento: new Date(),
+      id: 123,
+   };
+
+   beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+         providers: [
+            WebhookPagamentoPedidoUseCase,
+            {
+               provide: PagamentoConstants.IREPOSITORY,
+               useValue: {
+                  findBy: jest.fn(() => {
+                     return Promise.resolve([mockedPagamento]);
+                  }),
+                  edit: jest.fn(),
+               },
+            },
+            {
+               provide: PagamentoConstants.BUSCA_PEDIDO_ID_USECASE,
+               useValue: {
+                  buscarPedidoPorId: jest.fn(),
+               },
+            },
+            {
+               provide: PagamentoConstants.ATUALIZA_PEDIDO_COMO_RECEBIDO_USECASE,
+               useValue: {
+                  atualizarPedidoComoRecebido: jest.fn(),
+               },
+            },
+            {
+               provide: PagamentoConstants.WEBHOOK_PAGAMENTO_VALIDATOR,
+               useValue: mockedValidators,
+            },
+         ],
+      }).compile();
+
+      useCase = module.get<WebhookPagamentoPedidoUseCase>(WebhookPagamentoPedidoUseCase);
+      repository = module.get<IRepository<Pagamento>>(PagamentoConstants.IREPOSITORY);
+      buscaPedidoIdUseCase = module.get<BuscaPedidoIdUseCase>(PagamentoConstants.BUSCA_PEDIDO_ID_USECASE);
+      atualizaPedidoComoRecebidoUseCase = module.get<AtualizaPedidoComoRecebidoUseCase>(
+         PagamentoConstants.ATUALIZA_PEDIDO_COMO_RECEBIDO_USECASE
+      );
+   });
+
+   describe('webhook', () => {
+      it('should execute webhook successfully for a confirmed payment', async () => {
+         jest.spyOn(repository, 'findBy').mockResolvedValueOnce([mockedPagamento]);
+         jest.spyOn(atualizaPedidoComoRecebidoUseCase, 'atualizarPedidoComoRecebido').mockResolvedValueOnce(undefined);
+
+         const result = await useCase.webhook('123', 1);
+
+         expect(result).toBe(true);
+         expect(repository.edit).toHaveBeenCalledWith(mockedPagamento);
+      });
+
+      it('should throw a ServiceException if no associated payment is found', async () => {
+         jest.spyOn(repository, 'findBy').mockResolvedValueOnce([]);
+
+         await expect(useCase.webhook('123', 1)).rejects.toThrowError(ServiceException);
+         expect(repository.edit).not.toHaveBeenCalled();
+      });
+
+      it('should throw a ValidationException for an invalid payment state', async () => {
+         jest.spyOn(repository, 'findBy').mockResolvedValueOnce([mockedPagamento]);
+         jest.spyOn(atualizaPedidoComoRecebidoUseCase, 'atualizarPedidoComoRecebido').mockResolvedValueOnce(undefined);
+         mockedPagamento.estadoPagamento = EstadoPagamento.REJEITADO;
+
+         await expect(useCase.webhook('123', 4)).rejects.toThrowError(ValidationException);
+         expect(repository.edit).not.toHaveBeenCalled();
+      });
+
+      it('should update payment state and log when payment is confirmed', async () => {
+         jest.spyOn(repository, 'findBy').mockResolvedValueOnce([mockedPagamento]);
+         jest.spyOn(atualizaPedidoComoRecebidoUseCase, 'atualizarPedidoComoRecebido').mockResolvedValueOnce(undefined);
+
+         await useCase.webhook('123', 1);
+
+         expect(mockedPagamento.estadoPagamento).toEqual(EstadoPagamento.CONFIRMADO);
+         expect(repository.edit).toHaveBeenCalledWith(mockedPagamento);
+      });
+
+   });
+});
+
 describe('WebhookPagamentoPedidoUseCase', () => {
    it('criando um teste vazio para poder comentar o resto do código', () => {
       expect(true).toBeTruthy();
--- src/application/pagamento/validation/webhook-pagamento-pagamento-valido-validator.service.spec.ts
+++ src/application/pagamento/validation/webhook-pagamento-pagamento-valido-validator.service.spec.ts
@@ -1,5 +1,88 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EstadoPagamento } from 'src/enterprise/pagamento/enum/estado-pagamento.enum';
+import { Pagamento } from 'src/enterprise/pagamento/model/pagamento.model';
+import { IRepository } from 'src/enterprise/repository/repository';
+import { ServiceException } from 'src/enterprise/exception/service.exception';
+import { ValidationException } from 'src/enterprise/exception/validation.exception';
+import { PagamentoConstants } from 'src/shared/constants';
+import { WebhookPagamentoPagamentoValidoValidator } from 'src/application/pagamento/validation/webhook-pagamento-pagamento-valido-validator.service';
+
 describe('WebhookPagamentoPagamentoValidoValidator', () => {
-   it('criando um teste vazio para passar validação CI', () => {
-      expect(true).toBeTruthy();
+   let validator: WebhookPagamentoPagamentoValidoValidator;
+   let repository: IRepository<Pagamento>;
+
+   const mockedPagamento: Pagamento = {
+      pedidoId: 1,
+      transacaoId: '123',
+      estadoPagamento: EstadoPagamento.PENDENTE,
+      total: 100,
+      dataHoraPagamento: new Date(),
+      id: 123,
+   };
+
+   beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+         providers: [
+            WebhookPagamentoPagamentoValidoValidator,
+            {
+               provide: PagamentoConstants.IREPOSITORY,
+               useValue: {
+                  findBy: jest.fn(() => {
+                     return Promise.resolve([mockedPagamento]);
+                  }),
+               },
+            },
+         ],
+      }).compile();
+
+      validator = module.get<WebhookPagamentoPagamentoValidoValidator>(WebhookPagamentoPagamentoValidoValidator);
+      repository = module.get<IRepository<Pagamento>>(PagamentoConstants.IREPOSITORY);
+   });
+
+   describe('validate', () => {
+      it('should validate payment successfully if payment exists and not confirmed', async () => {
+         jest.spyOn(repository, 'findBy').mockResolvedValueOnce([mockedPagamento]);
+
+         const result = await validator.validate(mockedPagamento);
+
+         expect(result).toBe(true);
+      });
+
+      it('should throw a ValidationException if no associated payment is found', async () => {
+         jest.spyOn(repository, 'findBy').mockResolvedValueOnce([]);
+
+         await expect(validator.validate(mockedPagamento)).rejects.toThrowError(ServiceException);
+      });
+
+      it('should throw a ValidationException if payment status is already confirmed', async () => {
+
+         repository.findBy = jest.fn().mockImplementation(() => {
+            return Promise.resolve([{ ...mockedPagamento, estadoPagamento: EstadoPagamento.CONFIRMADO }]);
+         });
+
+         await expect(validator.validate(mockedPagamento)).rejects.toThrowError(ValidationException);
+      });
+
+      it('should handle errors when fetching payment', async () => {
+         jest.spyOn(repository, 'findBy').mockRejectedValueOnce(new Error('Failed to fetch payment'));
+
+         await expect(validator.validate(mockedPagamento)).rejects.toThrowError(ServiceException);
+      });
+
+      it('should handle null or undefined payment', async () => {
+         repository.findBy = jest.fn().mockImplementation(() => {
+            return Promise.resolve([undefined]);
+         });
+
+         await expect(validator.validate(mockedPagamento)).rejects.toThrowError(ServiceException);
+      });
+
+      it('should handle a payment with undefined transaction ID', async () => {
+         repository.findBy = jest.fn().mockImplementation((param) => {
+            return Promise.resolve(param.hasOwnProperty('transacaoId') ? [mockedPagamento] : []);
+         });
+         const pagamento: Pagamento = { ...mockedPagamento, transacaoId: undefined };
+         await expect(validator.validate(pagamento)).rejects.toThrowError(ValidationException);
+      });
    });
 });
