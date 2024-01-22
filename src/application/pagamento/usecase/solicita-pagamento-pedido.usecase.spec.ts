4488950b1cd08c7ef8f8a38eeeda5405df20a58c
Felipe Rosa <felipemax.suporteti@gmail.com>
2024-01-22 22:57:58 +0000

    test: new validations

--- src/application/pagamento/usecase/solicita-pagamento-pedido.usecase.spec.ts
+++ src/application/pagamento/usecase/solicita-pagamento-pedido.usecase.spec.ts
@@ -36,6 +36,22 @@ describe('SolicitaPagamentoPedidoUseCase', () => {
     ativo: true,
     id: 1,
     total: 10,
+    itensPedido: [
+      {
+        pedidoId: 1,
+        produtoId: 1,
+        quantidade: 1,
+        produto: {
+          preco: 10,
+          nome: 'Produto 1',
+          idCategoriaProduto: 1,
+          ativo: true,
+          descricao: 'Produto 1',
+          id: 1,
+          imagemBase64: '',
+        },
+      },
+    ],
   };
 
   beforeEach(async () => {
